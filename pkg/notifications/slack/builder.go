ee8eb76043939b3fa81a4130d00bd15144b3321e
Victor Chen <vchen2@atlassian.com>
2023-11-27 20:46:26 +1100

    Get slack token from file by default otherwise get it from env var

--- pkg/notifications/slack/builder.go
+++ pkg/notifications/slack/builder.go
@@ -3,21 +3,28 @@ package slack
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/atlassian-labs/cyclops/pkg/notifications"
 	slackapi "github.com/slack-go/slack"
 )
 
+const (
+	slackBotUserOAuthAccessTokenFile = "SLACK_BOT_USER_OAUTH_ACCESS_TOKEN_FILE"
+	slackBotUserOAuthAccessToken     = "SLACK_BOT_USER_OAUTH_ACCESS_TOKEN"
+	slackChannelID                   = "SLACK_CHANNEL_ID"
+)
+
 // NewNotifier returns a new Slack notifier
 func NewNotifier() (notifications.Notifier, error) {
 	// Return an error is no slack oauth token is provided
-	token, ok := os.LookupEnv("SLACK_BOT_USER_OAUTH_ACCESS_TOKEN")
+	token, ok := getSlackBotToken()
 	if !ok {
 		return nil, fmt.Errorf("missing slack oauth token")
 	}
 
-	// Return an error if no slack channel is specified
-	channelID, ok := os.LookupEnv("SLACK_CHANNEL_ID")
+	// Return an error if no Slack channel is specified
+	channelID, ok := getSlackChannelID()
 	if !ok {
 		return nil, fmt.Errorf("missing slack channel id")
 	}
@@ -27,10 +34,34 @@ func NewNotifier() (notifications.Notifier, error) {
 		channelID: channelID,
 	}
 
-	// Check that the slack app has been added to the channel in the workspace
+	// Check that the Slack app has been added to the channel in the workspace
 	if _, err := n.client.GetConversationInfo(channelID, false); err != nil {
 		return nil, err
 	}
 
 	return n, nil
+}
+
+func getSlackChannelID() (string, bool) {
+	// Read channel ID from env var
+	channelID, ok := os.LookupEnv(slackChannelID)
+	return channelID, ok
+}
+
+func getSlackBotToken() (string, bool) {
+	// Check if Slack token is provided as a file, file name is provided in env var
+	tokenFile, ok := os.LookupEnv(slackBotUserOAuthAccessTokenFile)
+
+	// If env var for token file is not set, read token from env var directly
+	if !ok {
+		token, ok := os.LookupEnv(slackBotUserOAuthAccessToken)
+		return token, ok
+	}
+
+	token, err := os.ReadFile(tokenFile)
+	if err != nil {
+		return "", false
+	}
+
+	return strings.TrimSpace(string(token)), true
 }
